2603bfcfda5463c2a3323be3b12101be21ffdf0c
caleb <56577750+kelly146@users.noreply.github.com>
2023-04-30 04:52:04 +0100

    more variables

--- prediction.R
+++ prediction.R
@@ -8,7 +8,20 @@ new_data <- data.frame(
   Squad="Real Madrid",
   Def3rdPres = 1500,
   Att3rdPres = 2000,
-  SaveRt = 70
+  SaveRt = 70,
+  TotSh = 580,
+  TotShTg =185,
+  TotTouc =23735,
+  DefPenTouc=2447,
+  Def3rdTouc=239,
+  Mid3rdTouc=18298,
+  Att3rdTouc= 2429,
+  AttPenTouc =6918, 
+  KP =7646,
+  SCAT =1092,
+  GCAT =100,
+  Def3rdPres=930,
+  Att3rdPres=1223
 )
 
 # Use the predict function to get the prediction
